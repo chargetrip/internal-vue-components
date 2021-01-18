d707b0dd1931788371e21c2583424eaa61bc5181
laurensvdmaas <laurensvdmaas@gmail.com>
2021-01-18 15:30:34 +0100

    fix(lib): tooltip triangles

--- src/components/tooltip/Tooltip.vue
+++ src/components/tooltip/Tooltip.vue
@@ -1,11 +1,11 @@
 <template>
   <div
-    class="c-tooltip z-5 flex flex-col rounded py-2 px-4 bg-base border border-alt2 shadow-down-sm absolute"
+    class="c-tooltip z-5 flex flex-col rounded py-2 px-4 bg-base border border-alt2 shadow-down-sm absolute transform"
     :class="{ left: orientation === 'left', bottom: orientation === 'bottom' }"
     @click.stop
   >
     <div
-      class="triangle w-2 h-2 border-r border-t absolute block bg-base border-alt2"
+      class="triangle w-2 h-2 border-r border-t absolute block bg-base border-alt2 transform"
     />
     <slot />
   </div>
@@ -31,23 +31,21 @@ export default class CTooltip extends Mixins(Base) {
 <style lang="scss">
 .c-tooltip {
   &.left {
-    @apply top-0 -mt-2 -ml-2 transform -translate-x-full left-0;
+    @apply top-0 -mt-2 -ml-2 -translate-x-full left-0;
 
     .triangle {
-      @apply transform rotate-45 mt-3 translate-x-full border-alt2;
+      @apply rotate-45 mt-3 translate-x-full border-alt2;
       right: 3px;
       top: -2px;
-      content: "";
     }
   }
 
   &.bottom {
-    @apply top-full left-1/2 transform -translate-x-1/2 mt-2;
+    @apply top-full left-1/2 -translate-x-1/2 mt-2;
 
     .triangle {
-      @apply transform -rotate-45 -translate-x-1/2 left-1/2;
+      @apply -rotate-45 -translate-x-1/2 left-1/2;
       top: -4px;
-      content: "";
     }
   }
 }
