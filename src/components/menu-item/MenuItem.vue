7a1d9bbb96cf034cda47056bf451ddd5d3ccf8ae
laurensvdmaas <laurensvdmaas@gmail.com>
2021-01-22 17:05:29 +0100

    fix: rel noopener menu item

--- src/components/menu-item/MenuItem.vue
+++ src/components/menu-item/MenuItem.vue
@@ -48,6 +48,7 @@ export default class CMenuItem extends Vue {
     } else if (this.href) {
       return {
         href: this.href,
+        rel: "noopener",
         target: this.target || "_blank",
         is: "a"
       };
