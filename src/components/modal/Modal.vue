20bd0c35bd04c231ec32ffe508b28ab30cfc263b
laurensvdmaas <laurensvdmaas@gmail.com>
2021-01-29 10:55:45 +0100

    fix: modal close

--- src/components/modal/Modal.vue
+++ src/components/modal/Modal.vue
@@ -1,16 +1,17 @@
 <template functional>
   <div
     class="c-modal transition leading-relaxed duration-300 flex fixed inset-0 z-40"
-    @click="$emit('close')"
     :class="[data.class, data.staticClass]"
   >
     <transition name="fade-in" appear>
-      <div class="bg absolute inset-0 bg-body opacity-80" />
+      <div
+        class="bg absolute cursor-pointer inset-0 bg-body opacity-80"
+        @click="$emit('close')"
+      />
     </transition>
     <transition name="slide-up" appear>
       <div
         class="modal-content bg-base border rounded relative z-10 p-6 shadow-down-sm m-auto border-alt2"
-        @click.stop
       >
         <slot />
       </div>
