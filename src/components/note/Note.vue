012b2e09ce28e18a1c9144946bb6a457bbb1439f
laurensvdmaas <laurensvdmaas@gmail.com>
2021-02-05 12:06:30 +0100

    fix: note styling

--- src/components/note/Note.vue
+++ src/components/note/Note.vue
@@ -1,9 +1,11 @@
 <template>
   <div
-    class="c-note border border-warning rounded p-6 flex items-start gap-3"
-    :class="{ 'flex-col': display === 'block' }"
+    class="c-note border border-warning rounded flex items-start gap-3 p-4"
+    :class="{
+      'flex-col': display === 'block'
+    }"
   >
-    <Tag color="warning" type="secondary" size="xs">Note</Tag>
+    <Tag color="warning" type="secondary" size="xs">{{ tag }}</Tag>
     <div class="text-font-alt">
       <slot />
     </div>
@@ -16,15 +18,16 @@ import Tag from "../tag/Tag.vue";
 @Component({ components: { Tag } })
 export default class extends Vue {
   @Prop({ default: "inline" }) display;
+  @Prop({ tag: "Note" }) tag;
 }
 </script>
 <style lang="scss">
 .theme-dark {
-  .note-comp {
+  .c-note {
     background: rgba(51, 22, 0, 0.2);
   }
 }
-.note-comp {
+.c-note {
   background: rgba(252, 215, 186, 0.2);
   padding-bottom: 1.5rem !important;
 }
