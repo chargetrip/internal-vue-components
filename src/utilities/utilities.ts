090fed5096ecac031847ab08591c2daae4e8dcd4
laurensvdmaas <laurensvdmaas@gmail.com>
2021-03-23 12:08:48 +0100

    fix: active state header

--- src/utilities/utilities.ts
+++ src/utilities/utilities.ts
@@ -5,3 +5,25 @@ export function normalizeHref(href) {
 
   return _href[_href.length - 1] === "/" ? _href : `${_href}/`;
 }
+
+export function normalizeLinkItem(item, origin, currentUrl) {
+  if (!(item.href || item.to)) return { ...item, target: "self" };
+
+  const href = item.href?.[0] === "/" ? `${origin}${item.href}` : item.href;
+  const normalizedHref = href?.[href?.length - 1] === "/" ? href : `${href}/`;
+  const normalizedTo =
+    item.to?.[item.to?.length - 1] === "/" ? item.to : `${item.to}/`;
+  const normalizedCurrentUrl =
+    currentUrl?.[currentUrl?.length - 1] === "/"
+      ? currentUrl
+      : `${currentUrl}/`;
+
+  return {
+    ...item,
+    href: normalizedHref,
+    target: "_self",
+    isLinkActive:
+      normalizedHref === normalizedCurrentUrl ||
+      `${origin}${normalizedTo}` === normalizedCurrentUrl
+  };
+}
