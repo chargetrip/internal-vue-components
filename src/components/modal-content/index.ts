8a159c755136a133dcb53317eb42eab70fe6c9e0
laurensvdmaas <laurensvdmaas@gmail.com>
2021-05-07 11:04:59 +0200

    feat: added modal-content & onboarding-content-container components

---
+++ src/components/modal-content/index.ts
@@ -0,0 +1,8 @@
+import ModalContent from "./ModalContent.vue";
+
+// @ts-ignore
+ModalContent.install = Vue => {
+  Vue.component(ModalContent.name, ModalContent);
+};
+
+export default ModalContent;
--- src/index.ts
+++ src/index.ts
@@ -1,110 +1,3 @@
-import ActionBar from "./components/action-bar";
-import Alert from "./components/alert";
-import Banner from "./components/banner";
-import Button from "./components/button";
-import Calendar from "./components/calendar";
-import CarList from "./components/car-list";
-import Card from "./components/card";
-import Cell from "./components/cell";
-import CombinedFormControl from "./components/combined-form-control";
-import CompactCard from "./components/compact-card";
-import Container from "./components/container";
-import Checkbox from "./components/checkbox";
-import CheckboxTree from "./components/checkbox-tree";
-import CheckboxTreeList from "./components/checkbox-tree-list";
-import CodeBlock from "./components/code-block";
-import Input from "./components/input";
-import Label from "./components/label";
-import Select from "./components/select";
-import SideNav from "./components/side-nav";
-import Switch from "./components/switch";
-import Upload from "./components/upload";
-import Footer from "./components/footer";
-import FormControl from "./components/form-control";
-import Image from "./components/image";
-import MenuItem from "./components/menu-item";
-import Modal from "./components/modal";
-import MonthInput from "./components/month-input";
-import Note from "./components/note";
-import QuickNav from "./components/quick-nav";
-import Radio from "./components/radio";
-import RadioTree from "./components/radio-tree";
-import Range from "./components/range";
-import Readonly from "./components/readonly";
-import Row from "./components/row";
-import Snackbar from "./components/snackbar";
-import Tab from "./components/tab";
-import Table from "./components/table";
-import Tag from "./components/tag";
-import Theme from "./components/theme";
-import TitleWithSubTitle from "./components/title-with-sub-title";
-import Tooltip from "./components/tooltip";
-import TopNav from "./components/top-nav";
-import WebsiteHeader from "./components/website-header";
-import Prefooter from "./components/prefooter";
-import HeightMap from "./components/height-map";
-
-const components = {
-  ActionBar,
-  Alert,
-  Banner,
-  Button,
-  Card,
-  Cell,
-  CompactCard,
-  Container,
-  Calendar,
-  CarList,
-  Checkbox,
-  CheckboxTree,
-  CheckboxTreeList,
-  CodeBlock,
-  Input,
-  Label,
-  Select,
-  SideNav,
-  Switch,
-  Upload,
-  CombinedFormControl,
-  Footer,
-  FormControl,
-  Image,
-  MenuItem,
-  Modal,
-  MonthInput,
-  Note,
-  QuickNav,
-  Radio,
-  RadioTree,
-  Range,
-  Readonly,
-  Row,
-  Snackbar,
-  Tab,
-  Table,
-  Tag,
-  Theme,
-  TitleWithSubTitle,
-  Tooltip,
-  TopNav,
-  WebsiteHeader,
-  Prefooter,
-  HeightMap
-};
-
-function install(Vue) {
-  for (const component in components) {
-    // tslint:disable-next-line: forin
-    Vue.component(components[component].name, components[component]);
-  }
-}
-
-if (typeof window !== "undefined" && window.Vue) {
-  install(window.Vue);
-}
-
-export default { install };
-
 export { default as ActionBar } from "./components/action-bar";
 export { default as Alert } from "./components/alert";
 export { default as Banner } from "./components/banner";
@@ -131,8 +24,10 @@ export { default as FormControl } from "./components/form-control";
 export { default as Image } from "./components/image";
 export { default as MenuItem } from "./components/menu-item";
 export { default as Modal } from "./components/modal";
+export { default as ModalContent } from "./components/modal-content";
 export { default as MonthInput } from "./components/month-input";
 export { default as Note } from "./components/note";
+export { default as OnboardingContentCOntainer } from "./components/onboarding-content-container";
 export { default as QuickNav } from "./components/quick-nav";
 export { default as Radio } from "./components/radio";
 export { default as RadioTree } from "./components/radio-tree";
