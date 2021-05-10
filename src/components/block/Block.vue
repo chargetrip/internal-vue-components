86224c27d354e3255410b653b78480f4b9160943
laurensvdmaas <laurensvdmaas@gmail.com>
2021-05-10 14:25:44 +0200

    feat: added list-item, app-section, block components

---
+++ src/components/block/Block.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="block rounded bg-base border border-alt">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {}
+})
+export default class extends Vue {}
+</script>
---
+++ src/components/list-item/ListItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li
+    class="c-list-item py-2 px-4 border border-alt rounded"
+    :class="{ 'border-dashed': isDashed }"
+  >
+    <slot />
+  </li>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class extends Vue {
+  @Prop() isDashed;
+}
+</script>
