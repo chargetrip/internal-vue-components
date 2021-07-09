11399b2e51cc2b7c738838e40692742c89a9de4b
laurensvdmaas <laurensvdmaas@gmail.com>
2021-07-09 11:29:25 +0200

    fix: new image component

--- src/modules/file-upload/plugin.js
+++ src/modules/file-upload/plugin.js
@@ -1,4 +1,4 @@
-import fileMap from "~/file-map.json";
+import fileMap from "~/file-map-cloudinary.json";
 export default (ctx, inject) => {
   ctx.$fileMap = fileMap;
   inject("fileMap", fileMap);
--- src/scripts/file-upload.mjs
+++ src/scripts/file-upload.mjs
@@ -1,98 +1,62 @@
-import { writeFileSync, readFileSync } from "fs";
-import dotenv from "dotenv";
-import { SiteClient } from "datocms-client";
-import * as chokidar from "chokidar";
-import consola from "consola";
-import imagemin from "imagemin";
-import imageminPngquant from "imagemin-pngquant";
+import fs, { readFileSync } from "fs";
+import cloudinary from "cloudinary";
 
-dotenv.config();
-const logger = consola.withScope(process.env.LOG_SCOPE);
-const client = new SiteClient(process.env.DATO_API_KEY);
-const actions = {
-  CREATE: "CREATE",
-  REMOVE: "REMOVE",
-  UPDATE: "UPDATE"
-};
-const allowedFormats = process.env?.ALLOWED_FORMATS?.split(",") || [];
-const watcher = chokidar.watch("static", {
-  ignored: /^\./,
-  persistent: true
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
-
-logger.success("Watching the following formats", process.env.ALLOWED_FORMATS);
-
 let map;
 
 try {
-  map = JSON.parse(readFileSync("./file-map.json", "utf8"));
+  map = JSON.parse(readFileSync("./file-map-cloudinary.json", "utf8"));
 } catch (e) {
   map = {};
 }
 
-const normalizePath = path => path.replace("static", "");
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const onChange = async (fullPath, stats, action) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
-  const [_, ext] = fullPath.split(".");
+const allowedFormats = ["jpg", "jpeg", "png", "mp4", "svg"];
+const loopDir = async (path, createFolder = true) => {
+  const files = await fs.readdirSync(path);
 
-  if (allowedFormats.includes(ext) && !fullPath.includes("/compressed/")) {
-    const normalizedPath = normalizePath(fullPath);
+  return Promise.all(
+    files.map(async fileName => {
+      const normalizedPath = `${path}/${fileName}`;
 
-    const id = map[normalizedPath]?.id;
+      const folder = path.replace("static", "");
 
-    if (action === actions.REMOVE) {
-      if (id) {
-        await client.uploads.destroy(id);
-        logger.info("File", normalizedPath, "has been removed");
-
-        delete map[normalizedPath];
-      }
-    }
-
-    if (action === actions.CREATE) {
-      if (!id) {
-        let destinationPath = fullPath;
-
-        if (ext === "png") {
-          const response = await imagemin([fullPath], {
-            destination: "static/compressed/",
-            plugins: [
-              imageminPngquant({
-                quality: [0.9, 1]
-              })
-            ]
-          });
-
-          if (!response) return;
-
-          destinationPath = response[0].destinationPath;
+      if (fs.lstatSync(normalizedPath).isDirectory()) {
+        if (createFolder) {
+          await cloudinary.v2.api.create_folder(folder);
         }
 
-        const path = await client.createUploadPath(destinationPath, {
-          filename: normalizedPath.split("/").join("-")
-        });
-        map[normalizedPath] = await client.uploads.create({
-          path
-        });
-        logger.info("File", normalizedPath, "has been created");
+        await loopDir(normalizedPath);
+      } else {
+        const ext = fileName.split(".")[1];
+
+        if (allowedFormats.includes(ext)) {
+          const response = await cloudinary.v2.uploader
+            .upload(normalizedPath, {
+              folder: createFolder ? folder : "",
+              resource_type: "auto",
+              use_filename: true,
+              unique_filename: false
+            })
+            .catch(console.log);
+
+          if (response) {
+            const { width, height } = response;
+
+            map[`${createFolder ? folder : ""}/${fileName}`] = {
+              width,
+              height
+            };
+          }
+        }
       }
-    }
-
-    if (action === actions.UPDATE) {
-      if (id) {
-        await onChange(fullPath, stats, actions.REMOVE);
-        await onChange(fullPath, stats, actions.CREATE);
-
-        logger.info("File", normalizedPath, "has been updated");
-      }
-    }
-
-    return writeFileSync("./file-map.json", JSON.stringify(map));
-  }
+    })
+  );
 };
 
-watcher
-  .on("add", (path, stats) => onChange(path, stats, actions.CREATE))
-  .on("change", (path, stats) => onChange(path, stats, actions.UPDATE))
-  .on("unlink", (path, stats) => onChange(path, stats, actions.REMOVE));
+await loopDir("static", false);
+
+fs.writeFileSync("file-map-cloudinary.json", JSON.stringify(map));
