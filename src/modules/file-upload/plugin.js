c66fc4d22b08fef41a1142258c6d199448b6685e
laurensvdmaas <laurensvdmaas@gmail.com>
2021-09-01 14:49:17 +0200

    fix: image include folder path

--- src/modules/file-upload/plugin.js
+++ src/modules/file-upload/plugin.js
@@ -1,5 +1,7 @@
 import fileMap from "~/file-map-cloudinary.json";
 export default (ctx, inject) => {
   ctx.$fileMap = fileMap;
+  // eslint-disable-next-line no-undef
+  ctx.$cloudinaryFolder = options.CLOUDINARY_FOLDER;
   inject("fileMap", fileMap);
 };
