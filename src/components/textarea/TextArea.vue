fcbe3ef3e6a924c50ccb55140ca566642011feb1
Jason Miller <jason.miller@chargetrip.com>
2021-09-30 07:58:32 +0200

    feat: add TextArea component

---
+++ src/components/textarea/TextArea.vue
@@ -0,0 +1,173 @@
+<template>
+  <FormControl
+    class="c-textarea"
+    v-bind="$props"
+    :label-inside="true"
+    :focus="focus"
+    :label-always-visible="false"
+    @click.native="() => (disabled ? null : textarea.focus())"
+    @hover="$emit('hover', $event)"
+    :class="{
+      'has-prefix': prefix,
+      'has-value': hasValue,
+      'has-focus': focus,
+      'has-label': label
+    }"
+  >
+    <div class="flex box">
+      <div class="prefix" v-if="prefix" v-html="prefix" />
+      <div
+        class="icon h-auto flex items-center text-font-alt3 pl-2"
+        v-if="icon"
+        :class="`icon-${icon}`"
+      />
+      <div class="flex flex-col relative flex-1">
+        <label
+          class="pointer-events-none"
+          :for="id"
+          v-if="label"
+          v-html="label"
+        />
+        <textarea
+          ref="textarea"
+          rows="1"
+          :value="value"
+          :id="id"
+          :name="name"
+          :maxlength="maxlength"
+          :autocomplete="autocomplete"
+          :readonly="readonly || disabled"
+          :placeholder="placeholder"
+          @keyup.stop
+          @mouseenter="setHover(true)"
+          @mouseleave="setHover(false)"
+          @focus="setFocus(true)"
+          @paste="$emit('paste', $event)"
+          @blur="onBlur"
+          @input="onInput"
+        />
+        <span
+          class="icon-checkmark absolute top-1/2 transform text-16 -translate-y-1/2 right-0 mr-3 text-accent"
+          v-if="showCheckmark && validation && !validation.$invalid"
+        />
+      </div>
+      <div
+        class="icon hotkey bg-alt2 flex items-center justify-center w-5 h-5 rounded-sm my-auto mr-1"
+        :class="`icon-${hotkey.icon}`"
+        v-if="hotkey"
+      />
+      <div class="suffix" v-if="suffix">
+        {{ suffix }}
+      </div>
+    </div>
+  </FormControl>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Ref, Emit } from "vue-property-decorator";
+import { Listen } from "@/utilities/decorators";
+import FormControl from "@/components/form-control/FormControl.vue";
+import { FormControlProps } from "@/utilities/utilities";
+
+@Component({
+  components: { FormControl }
+})
+export default class CTextArea extends FormControlProps {
+  @Prop() public maxlength!: number;
+  @Prop() public resizable!: boolean;
+  @Ref("textarea") public textarea!: HTMLTextAreaElement;
+  public focus = false;
+  public hover = false;
+
+  @Listen("keyup") public onKeyDown(e) {
+    e.preventDefault();
+  }
+
+  @Listen("keypress") public onKeypress(e) {
+    if (!this.hotkey) return;
+
+    if (e.key === this.hotkey.key) {
+      e.preventDefault();
+      this.hotkey.fn(this.textarea);
+    }
+  }
+
+  public onInput(event: InputEvent) {
+    const value = event.target.value;
+    // const value = this.max
+    //   ? Math.min(e.target.value, this.max).toString()
+    //   : e.target.value;
+
+    // if (this.type === "number") {
+    //   const parsed = parseFloat(value.replace(",", "."));
+
+    //   const replacedValue = parsed.toString().replace(/[^0-9(.|,)]/g, "");
+
+    //   const normalizedValue = (value[value.length - 1] === "."
+    //     ? `${replacedValue}.`
+    //     : replacedValue
+    //   ).slice(0, this.maxlength || Infinity);
+
+    //   e.target.value = normalizedValue;
+
+    //   return this.$emit("input", normalizedValue || null, e);
+    // }
+
+    this.$emit(
+      "input",
+      this.maxlength ? value.slice(0, this.maxlength) : value,
+      event
+    );
+  }
+
+  @Emit("focus") public setFocus(val) {
+    this.focus = val;
+    return val;
+  }
+
+  @Emit("blur")
+  @Emit("input")
+  public onBlur(event) {
+    this.setFocus(false);
+
+    console.log("event (blur|input), setFocus = false");
+
+    return event.target.value;
+  }
+
+  @Emit("hover") public setHover(val) {
+    this.hover = val;
+    return val;
+  }
+
+  get hasValue() {
+    return (
+      (this.value && this.value.toString().length) ||
+      this.focus ||
+      this.placeholder
+    );
+  }
+}
+</script>
+
+<style lang="scss">
+.c-textarea {
+  @apply bg-none;
+
+  &[disabled] {
+    textarea {
+      @apply text-font-alt3 opacity-50 pointer-events-none;
+    }
+  }
+  &.has-label {
+    &.label-always-visible {
+      .hotkey {
+        @apply mr-3;
+      }
+      textarea {
+        @apply mt-6;
+      }
+    }
+  }
+}
+</style>
---
+++ src/stories/TextArea.stories.js
@@ -0,0 +1,101 @@
+import Theme from "../components/theme/Theme.vue";
+import TextArea from "../components/textarea/TextArea.vue";
+
+import "../assets/styles/index.scss";
+import { icons } from "./utils";
+import "./storybook.css";
+import { validationMixin } from "vuelidate";
+import { required } from "vuelidate/lib/validators";
+
+export default {
+  title: "Form/TextArea",
+  component: TextArea,
+  argTypes: {
+    placeholder: { control: { type: "text" } },
+    label: { control: { type: "text" } },
+    prefix: { control: { type: "text" } },
+    suffix: { control: { type: "text" } },
+    icon: { control: { type: "select", options: icons } },
+    hotkey: { control: { type: "object" } },
+    disabled: { control: { type: "boolean" } },
+    darkMode: { control: { type: "boolean" } }
+  }
+};
+
+const Template = (args, { argTypes }) => {
+  return {
+    props: Object.keys(argTypes),
+    components: { TextArea, Theme },
+    mixins: [validationMixin],
+    data() {
+      return {
+        errorMessages: {
+          minLength: "14 or more characters",
+          specialCharacter: "Contains a special character",
+          lowerCharacter: "Contains a lowercase character",
+          upperCharacter: "Contains an uppercase character"
+        },
+        form: {
+          textarea1: null,
+          textarea2: "",
+          textarea3: ""
+        }
+      };
+    },
+    validations: {
+      form: {
+        textarea1: {
+          minLength: value => value?.length >= 14,
+          required
+        },
+        textarea2: {
+          minLength: value => value?.length >= 14,
+          required
+        },
+        textarea3: {
+          required
+        }
+      }
+    },
+    methods: {
+      submit() {
+        this.$v.$touch();
+      }
+    },
+    template: `<Theme :dark-mode="darkMode">
+      <div class="grid grid-cols-1 gap-2 content-start max-w-lg">
+        <TextArea
+          v-model="$v.form.textarea1.$model"
+          :rules="errorMessages"
+          :error-message="null"
+          :form="$v"
+          :validation="$v.form.textarea1"
+          v-bind="$props"
+          rules-title="Example rules"
+        />
+        <TextArea
+          v-model="$v.form.textarea2.$model"
+          :validation="$v.$dirty && $v.form.textarea2"
+          error-message="Error!@"
+          v-bind="$props"
+        />
+        <TextArea
+          v-model="$v.form.textarea3.$model"
+          :validation="$v.$dirty && $v.form.textarea3"
+          error-message="Error!@"
+          v-bind="$props"
+        />
+      </div>
+    </Theme>`
+  };
+};
+
+export const Label = Template.bind({});
+Label.args = {
+  label: "Label"
+};
+
+export const Placeholder = Template.bind({});
+Placeholder.args = {
+  placeholder: "You're on! Provide us with some background information."
+};
