b605fcb6a09e3b7ea093d556b295cd09a48dc84a
laurensvdmaas <laurensvdmaas@gmail.com>
2021-11-10 15:49:02 +0100

    fix: onboarding container h-full

--- src/components/onboarding-content-container/OnboardingContentContainer.vue
+++ src/components/onboarding-content-container/OnboardingContentContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="onboarding-content-container px-6 flex text-center !mb-0">
+  <div class="onboarding-content-container h-full px-6 flex text-center !mb-0">
     <div class="content py-12 w-full m-auto flex flex-col justify-between">
       <span class="icon text-48 mb-7" v-if="icon" :class="`icon-${icon}`" />
       <h3>
