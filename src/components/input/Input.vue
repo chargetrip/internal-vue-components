2ec8cc668257571d2bf8c4132902583d9ba52bc2
Jason Miller <contact@jasonmiller.nl>
2021-12-16 17:20:50 +0100

    fix: center icon using h-auto, remove double opacity

--- src/components/input/Input.vue
+++ src/components/input/Input.vue
@@ -17,7 +17,7 @@
     <div class="flex box">
       <div class="prefix" v-if="prefix" v-html="prefix" />
       <div
-        class="icon h-full flex items-center text-font-alt3 pl-2"
+        class="icon h-auto flex items-center text-font-alt3 pl-2"
         v-if="icon"
         :class="`icon-${icon}`"
       />
@@ -163,7 +163,7 @@ export default class CInput extends FormControlProps {
 .c-input {
   &[disabled] {
     input {
-      @apply text-font-alt3 opacity-50 opacity-50 pointer-events-none;
+      @apply text-font-alt3 opacity-50 pointer-events-none;
     }
   }
   &.has-label {
