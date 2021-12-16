d9312dc5a42bd0ac3905c0904da055efaedea51e
Jason Miller <contact@jasonmiller.nl>
2021-12-16 17:22:19 +0100

    feat: add TextArea component

--- src/index.ts
+++ src/index.ts
@@ -58,6 +58,7 @@ export { default as TopNav } from "./components/top-nav";
 export { default as WebsiteHeader } from "./components/website-header";
 export { default as Prefooter } from "./components/prefooter";
 export { default as HeightMap } from "./components/height-map";
+export { default as TextArea } from "./components/text-area";
 
 //directives
 export { default as LazyLoadDirective } from "./directives/lazy-load";
---
+++ src/stories/TextArea.stories.js
@@ -0,0 +1,110 @@
+import Theme from "../components/theme/Theme.vue";
+import TextArea from "../components/text-area/TextArea.vue";
+
+import "../assets/styles/index.scss";
+import { icons } from "./utils";
+import "./storybook.css";
+import { validationMixin } from "vuelidate";
+import { required } from "vuelidate/lib/validators";
+
+export default {
+  title: "Form/TextArea",
+  component: TextArea,
+  argTypes: {
+    placeholder: { control: { type: "text" } },
+    label: { control: { type: "text" } },
+    prefix: { control: { type: "text" } },
+    suffix: { control: { type: "text" } },
+    icon: { options: icons, control: { type: "select" } },
+    hotkey: { control: { type: "object" } },
+    disabled: { control: { type: "boolean" } },
+    darkMode: { control: { type: "boolean" } },
+    autoresize: {
+      control: { type: "boolean" },
+      description: "Dynamically resize TextArea as text grows larger/smaller"
+    },
+    rows: {
+      control: { type: "number", min: 1, step: 1 },
+      description: "Number of default rows for the TextArea"
+    }
+  }
+};
+
+const Template = (args, { argTypes }) => {
+  return {
+    props: Object.keys(argTypes),
+    components: { TextArea, Theme },
+    mixins: [validationMixin],
+    data() {
+      return {
+        errorMessages: {
+          minLength: "14 or more characters",
+          specialCharacter: "Contains a special character",
+          lowerCharacter: "Contains a lowercase character",
+          upperCharacter: "Contains an uppercase character"
+        },
+        form: {
+          textarea1: null,
+          textarea2: "",
+          textarea3: ""
+        }
+      };
+    },
+    validations: {
+      form: {
+        textarea1: {
+          minLength: value => value?.length >= 14
+        },
+        textarea2: {
+          minLength: value => value?.length >= 14,
+          required
+        },
+        textarea3: {
+          required
+        }
+      }
+    },
+    methods: {
+      submit() {
+        this.$v.$touch();
+      }
+    },
+    template: `<Theme :dark-mode="darkMode">
+      <div class="grid grid-cols-1 gap-2 content-start max-w-lg">
+        <TextArea
+          v-model="$v.form.textarea1.$model"
+          :rules="errorMessages"
+          :error-message="null"
+          :form="$v"
+          :validation="$v.form.textarea1"
+          v-bind="$props"
+          rules-title="Example rules"
+        />
+        <TextArea
+          v-model="$v.form.textarea2.$model"
+          :validation="$v.$dirty && $v.form.textarea2"
+          error-message="Error!@"
+          v-bind="$props"
+        />
+        <TextArea
+          v-model="$v.form.textarea3.$model"
+          :validation="$v.$dirty && $v.form.textarea3"
+          error-message="Error!@"
+          v-bind="$props"
+        />
+      </div>
+    </Theme>`
+  };
+};
+
+export const Label = Template.bind({});
+Label.args = {
+  label: "Label",
+  rows: 1
+};
+
+export const Placeholder = Template.bind({});
+Placeholder.args = {
+  placeholder: "You're on! Provide us with some background information.",
+  rows: 1
+};
