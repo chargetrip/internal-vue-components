86a90dd9ee182a593cee72eda9905337f52808ef
laurensvdmaas <laurensvdmaas@gmail.com>
2022-01-10 10:09:04 +0100

    fix: calendar show current value

--- src/filters/date.ts
+++ src/filters/date.ts
@@ -1,5 +1,9 @@
 import { format } from "date-fns";
 
 export default (value: Date | string, formatStr: string) => {
-  return format(value instanceof Date ? value : new Date(value), formatStr);
+  try {
+    return format(value instanceof Date ? value : new Date(value), formatStr);
+  } catch {
+    return value;
+  }
 };
