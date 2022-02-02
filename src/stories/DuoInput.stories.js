3d77717060c4d06ce1fdbfc0a64e86646ece66dd
laurensvdmaas <laurensvdmaas@gmail.com>
2022-02-02 14:55:01 +0100

    fix: block duo styling

--- src/stories/DuoInput.stories.js
+++ src/stories/DuoInput.stories.js
@@ -1,5 +1,6 @@
 import Theme from "../components/theme/Theme.vue";
 import Button from "../components/button/Button.vue";
+import Block from "../components/block/Block.vue";
 import DuoInput from "../components/duo-input/DuoInput.vue";
 import "../assets/styles/index.scss";
 import { required, minLength } from "vuelidate/lib/validators";
@@ -45,10 +46,17 @@ const Template = (args, { argTypes }) => {
         }
       }
     }),
-    components: { Button, DuoInput, Theme },
+    components: { Button, DuoInput, Theme, Block },
     template: `<Theme :dark-mode="darkMode">
       <div class="space-y-8">
-        <DuoInput @blur="$v.form.$touch" error-message="Some error message" :validation="$v.form" :input1="form.input1" :input2="form.input2" @input1="form.input1.value = $event" @input2="form.input2.value = $event" v-bind="$props" />
+        <div class="flex space-x-4">
+          <div class="div">
+            <DuoInput @blur="$v.form.$touch" error-message="Some error message" :validation="$v.form" :input1="form.input1" :input2="form.input2" @input1="form.input1.value = $event" @input2="form.input2.value = $event" v-bind="$props" />
+          </div>
+          <Block :style="{backgroundColor: 'transparent'}">
+            <DuoInput @blur="$v.form.$touch" error-message="Some error message" :validation="$v.form" :input1="form.input1" :input2="form.input2" @input1="form.input1.value = $event" @input2="form.input2.value = $event" v-bind="$props" />
+          </Block>
+        </div>
         <Button size="sm" type="primary" @click.native="$v.form.$touch">
           Submit
         </Button>
